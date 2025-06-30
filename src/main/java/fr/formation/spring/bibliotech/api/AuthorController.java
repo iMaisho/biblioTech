7f4648676ef5d8d06ccf32acd9cbe5dcb45130e8
iMaisho <antonin.mingam@gmail.com>
2025-06-30 15:21:55 +0200

    Ajoute les fonctions CRUD à Authors

--- src/main/java/fr/formation/spring/bibliotech/api/AuthorController.java
+++ src/main/java/fr/formation/spring/bibliotech/api/AuthorController.java
@@ -1,12 +1,15 @@
 package fr.formation.spring.bibliotech.api;
 
 import fr.formation.spring.bibliotech.dal.entities.Author;
+import fr.formation.spring.bibliotech.dal.entities.Book;
 import fr.formation.spring.bibliotech.dal.repositories.AuthorRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 // @RestController combine @Controller et @ResponseBody.
 // Il indique que cette classe gère des requêtes web et que les
@@ -32,4 +35,55 @@ public class AuthorController {
         // enregistrements.
         return this.authorRepository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Author> getAuthorById(@PathVariable Long id) {
+        // Spring Data JPA nous offre findAll() pour récupérer tous les
+        // enregistrements.
+        Optional<Author> author = this.authorRepository.findById(id);
+
+        if (author.isPresent()) {
+            return ResponseEntity.ok(author.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Author> createAuthor(@RequestBody Author authorToCreate) {
+
+        Author savedAuthor = this.authorRepository.save(authorToCreate);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest() // Prend l'URL de la requête actuelle (/api/books)
+                .path("/{id}")        // Ajoute /{id}
+                .buildAndExpand(savedAuthor.getId()) // Remplace {id} par l'ID du livre créé
+                .toUri();
+
+        return ResponseEntity.created(location).body(savedAuthor);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Author> updateAuthor(@PathVariable Long id, @RequestBody Author updatedAuthor) {
+        Optional<Author> authorToUpdate = this.authorRepository.findById(id);
+        if (authorToUpdate.isPresent()) {
+            authorToUpdate.get().setFirstName(updatedAuthor.getFirstName());
+            authorToUpdate.get().setLastName(updatedAuthor.getLastName());
+            return ResponseEntity.ok(this.authorRepository.save(authorToUpdate.get()));
+        }
+        else  {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAuthor(@PathVariable Long id) {
+
+        if (authorRepository.existsById(id)) {
+            this.authorRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        else  {
+            return ResponseEntity.notFound().build();
+        }
+}
 }
